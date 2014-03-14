706369f174e64a5ce0fced1abb4c8df54a9bdaf5
Cox <truelonghornfan@comcast.net>
2014-03-14 09:03:01 -0600

    Got bubble and selection working, get insertion to work

--- Sorting.java
+++ Sorting.java
@@ -1,10 +1,12 @@
 public class Sorting{
 	
 	public static void main(String[] args) {
-		int[] test = {8,3,7,8,7,1,0};
+		int[] test = {3,1,6,7,1,2};
+		int[] test1 = {3,1,6,7,1,2};
+		int[] test2 = {3,1,6,7,1,2};
 		int[] fixed = selectionSort(test);
-		int[] fixed1 = insertionSort(test);
-		int[] fixed2 = bubbleSort(test);
+		int[] fixed1 = insertionSort(test1);
+		int[] fixed2 = bubbleSort(test2);
 		
 		if (isSorted(fixed)) {
 			System.out.println("Selection");
@@ -14,59 +16,62 @@ public class Sorting{
 			System.out.println("Insertion");
 			print(fixed1);
 		}
-		// if (isSorted(fixed2)) {
-		// 	System.out.println("Bubble");
-		// 	print(fixed2);
-		// }
+		if (isSorted(fixed2)) {
+			System.out.println("Bubble");
+			print(fixed2);
+		}
 
 	}
 
 	public static int[] selectionSort(int[] test){
-		for (int i=0, k = test.length-1; i<test.length; i++, k--) {
-			for (int j=0; j<test.length; j++) {
-				if (test[j] < test[i]) {
-					int first = test[i];
-					test[i] = test[j];
-					test[j] = first;
+		int placeHolder = 0;
+		for (int i=0; i<test.length; i++) {
+			int min = test[i];
+			for (int k=i; k<test.length; k++) {
+				if (test[k] < min) {
+					min = test[k];
+					placeHolder = k;
 				}
 			}
-			int second = test[i];
-			test[i] = test[k];
-			test[k] = second;
-			print(test);
-
-
+			int temp = test[placeHolder];
+			test[placeHolder] = test[i];
+			test[i] = temp;
+			
+			
 		}
 		return test;
 	}
 
-	public static int[] insertionSort(int[] test){
-		for (int i=0; i<test.length-1 ; i++) {
-			if (test[i] > test[i+1]) {
-				int first = test[i];
-				test[i] = test[i+1];
-				test[i+1] = first;
-				
+	public static int[] insertionSort(int[] test1){
+		int min = 0;
+		for (int i=1; i<test1.length; i++) {
+			min = i;
+			for (int x=1-1; x>=0; x--) {
+				if (test1[i] < test1[x]) {
+					int holder = test1[x];
+					test1[x] = test1[i];
+					test1[i] = holder;
+				}
+				i--;
 			}
-			print(test);
+			i = min;
 		}
-		return test;
+		return test1;
 
 
 	}
 
-	public static int[] bubbleSort(int[] test){
-		for (int i=0; i<test.length-1 ; i++) {
-			if (test[i] > test[i+1]) {
-				int first = test[i];
-				test[i] = test[i+1];
-				test[i+1] = first;
-				print(test);
+	public static int[] bubbleSort(int[] test2){
+		for (int i=0; i<test2.length-1; i++) {
+			for (int x=0; x<test2.length-1; x++) {
+				if (test2[x] > test2[x+1]) {
+					int temp = test2[x+1];
+					test2[x+1] = test2[x];
+					test2[x] = temp;
+				}
 			}
-
 		}
-		return test;
-
+		return test2;
 	}
 
 
